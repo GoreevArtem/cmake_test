969b2b07d8c892f6764f4e07bc0a23b913e2f1d1
GoreevArtoum <goreevartoum@gmail.com>
2021-11-02 01:58:14 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string>
 
-int main(int, char**){
-	std::cout<<"Hello from Git example"<<std::endl;
-	std::cout<<"Second line"<<std::endl;
-	std::cout<<5+10<<std::endl;
-	return 0;
+int main(int, char **)
+{
+    std::cout << "hello world from the second example" << std::endl;
+    //std::cout << "Version = " << examples::getVersions() << std::endl;
+    int var = 2;
+    int& ref = var;
+    std::cout<<" ref "<<ref<<std::endl;
+    ++var;
+    std::cout<<" var "<<var<<std::endl;
+    std::cout<<" ref "<<ref<<std::endl;
+    ++ref;
+    std::cout<<" var "<<var<<std::endl;
+    std::cout<<" ref "<<ref<<std::endl;
+    getchar();
+    return 0;
 }
-
